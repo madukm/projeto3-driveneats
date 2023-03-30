9e86d1b448d36192c43f244e8f463fe3359dcab4
Madu <kawakamimadu@gmail.com>
2023-03-30 11:28:12 -0300

    Selecting option done
    To do:
    Close order

---
+++ script.js
@@ -0,0 +1,54 @@
+function selectFirstDish() {
+    document.getElementById("first-dish").style.borderColor = "#32B72F";
+    document.getElementById("second-dish").style.borderColor = "white";
+    document.getElementById("third-dish").style.borderColor = "white";
+}
+
+function selectSecondDish() {
+    document.getElementById("first-dish").style.borderColor = "white";
+    document.getElementById("second-dish").style.borderColor = "#32B72F";
+    document.getElementById("third-dish").style.borderColor = "white";
+}
+
+function selectThirdDish() {
+    document.getElementById("first-dish").style.borderColor = "white";
+    document.getElementById("second-dish").style.borderColor = "white";
+    document.getElementById("third-dish").style.borderColor = "#32B72F";
+}
+
+function selectFirstDrink() {
+    document.getElementById("first-drink").style.borderColor = "#32B72F";
+    document.getElementById("second-drink").style.borderColor = "white";
+    document.getElementById("third-drink").style.borderColor = "white";
+}
+
+function selectSecondDrink() {
+    document.getElementById("first-drink").style.borderColor = "white";
+    document.getElementById("second-drink").style.borderColor = "#32B72F";
+    document.getElementById("third-drink").style.borderColor = "white";
+}
+
+function selectThirdDrink() {
+    document.getElementById("first-drink").style.borderColor = "white";
+    document.getElementById("second-drink").style.borderColor = "white";
+    document.getElementById("third-drink").style.borderColor = "#32B72F";
+}
+
+function selectFirstDessert() {
+    document.getElementById("first-dessert").style.borderColor = "#32B72F";
+    document.getElementById("second-dessert").style.borderColor = "white";
+    document.getElementById("third-dessert").style.borderColor = "white";
+}
+
+function selectSecondDessert() {
+    document.getElementById("first-dessert").style.borderColor = "white";
+    document.getElementById("second-dessert").style.borderColor = "#32B72F";
+    document.getElementById("third-dessert").style.borderColor = "white";
+}
+
+function selectThirdDessert() {
+    document.getElementById("first-dessert").style.borderColor = "white";
+    document.getElementById("second-dessert").style.borderColor = "white";
+    document.getElementById("third-dessert").style.borderColor = "#32B72F";
+}
+
