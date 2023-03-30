1aecd49b571120d8694cf848cf786a66b42761a0
Madu <kawakamimadu@gmail.com>
2023-03-30 16:20:59 -0300

    Finishing order working

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let dish, drink, dessert;
 
+var dishPrice = 0;
+var drinkPrice = 0;
+var dessertPrice = 0;
+
 function selectFirstDish() {
     document.getElementById("first-dish").style.borderColor = "#32B72F";
     document.getElementById("second-dish").style.borderColor = "white";
     document.getElementById("third-dish").style.borderColor = "white";
     dish = "first";
+    dishPrice = 14.90;
     if(dish != null && drink != null && dessert != null) {
         enableButton();
     }
@@ -15,6 +20,7 @@ function selectSecondDish() {
     document.getElementById("second-dish").style.borderColor = "#32B72F";
     document.getElementById("third-dish").style.borderColor = "white";
     dish = "second";
+    dishPrice = 18.90;
     if(dish != null && drink != null && dessert != null) {
         enableButton();
     }
@@ -25,6 +31,7 @@ function selectThirdDish() {
     document.getElementById("second-dish").style.borderColor = "white";
     document.getElementById("third-dish").style.borderColor = "#32B72F";
     dish = "third";
+    dishPrice = 16.90;
     if(dish != null && drink != null && dessert != null) {
         enableButton();
     }
@@ -35,6 +42,7 @@ function selectFirstDrink() {
     document.getElementById("second-drink").style.borderColor = "white";
     document.getElementById("third-drink").style.borderColor = "white";
     drink = "first";
+    drinkPrice = 4.90;
     if(dish != null && drink != null && dessert != null) {
         enableButton();
     }
@@ -45,6 +53,7 @@ function selectSecondDrink() {
     document.getElementById("second-drink").style.borderColor = "#32B72F";
     document.getElementById("third-drink").style.borderColor = "white";
     drink = "second";
+    drinkPrice = 3.90;
     if(dish != null && drink != null && dessert != null) {
         enableButton();
     }
@@ -55,6 +64,7 @@ function selectThirdDrink() {
     document.getElementById("second-drink").style.borderColor = "white";
     document.getElementById("third-drink").style.borderColor = "#32B72F";
     drink = "third";
+    drinkPrice = 3.90;
     if(dish != null && drink != null && dessert != null) {
         enableButton();
     }
@@ -65,6 +75,7 @@ function selectFirstDessert() {
     document.getElementById("second-dessert").style.borderColor = "white";
     document.getElementById("third-dessert").style.borderColor = "white";
     dessert = "first";
+    dessertPrice = 7.90;
     if(dish != null && drink != null && dessert != null) {
         enableButton();
     }
@@ -75,6 +86,7 @@ function selectSecondDessert() {
     document.getElementById("second-dessert").style.borderColor = "#32B72F";
     document.getElementById("third-dessert").style.borderColor = "white";
     dessert = "second";
+    dessertPrice = 8.90;
     if(dish != null && drink != null && dessert != null) {
         enableButton();
     }
@@ -85,6 +97,7 @@ function selectThirdDessert() {
     document.getElementById("second-dessert").style.borderColor = "white";
     document.getElementById("third-dessert").style.borderColor = "#32B72F";
     dessert = "third";
+    dessertPrice = 9.90;
     if(dish != null && drink != null && dessert != null) {
         enableButton();
     }
@@ -100,5 +113,9 @@ function enableButton() {
 }
 
 function sendOrder() {
+    let total = dishPrice + drinkPrice + dessertPrice;
+    let message = encodeURIComponent("Olá, gostaria de fazer o pedido:\n- Prato: Frango Yin Yang\n- Bebida: Coquinha Gelada- Sobremesa: Pudim\nTotal: R$ " + total.toFixed(2));
+    let link = "https://wa.me/5599999999999?text=" + message;
+    window.open(link, "_self");
     // mandar para whatsapp
 }
